c8e297bc2171b4279b4122cc84533f97e03fd483
Jonathan Hsu <jonhsu.design@gmail.com>
2020-04-15 18:41:41 -0700

    finished stretch goal: added search functionality

--- src/App.js
+++ src/App.js
@@ -6,19 +6,36 @@ import Heading from './Heading';
 const url = 'https://api.nasa.gov/planetary/apod';
 const api_key = 'kRfwFSln261qa5kxxPvHAKAHb3azNosA3kLe8dt0';
 
+// Setting min and max dates on calendar for fetching data from API
+var origin = new Date('Fri Jun 16 1995 00:00:00 GMT-0700 (Pacific Daylight Time)').toISOString().slice(0, 10);
+var local = (new Date()).getTimezoneOffset() * 60000;
+var today = (new Date(Date.now() - local)).toISOString().slice(0, 10);
+
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState([])
+  const [search, setSearch] = useState('');
+  const [date, setDate] = useState(today);
+
+  const newSearch = event => {
+    setSearch(event.target.value)
+  }
+
+  const getSearch = event => {
+    // prevent page from reloading every search
+    event.preventDefault();
+    setDate(search);
+  }
 
   useEffect(() => {
-    axios.get(`${url}?api_key=${api_key}`)
+    axios.get(`${url}?api_key=${api_key}&date=${date}`)
       .then(res => {
         console.log(res.data);
         setData(res.data)
       })
       .catch(err => {
-        alert('The NASA Image of the Day could not be retrieved.')
+        
       })
-  }, [])
+  }, [date])
 
   if(!data) {
     return null;
@@ -28,10 +45,16 @@ function App() {
       {/* <p>
         Read through the instructions in the README.md file to build your NASA app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p> */}
-      <h1>🚀 NASA Astronomy Picture of the Day 🚀</h1>
+      <h1><span role="img" aria-label='go!'>🚀</span> NASA Astronomy Picture of the Day <span role="img" aria-label='go!'>🚀</span></h1>
+
+      <form onSubmit={getSearch}>
+        <input className='calendar' type='date' value={search} onChange={newSearch} min={origin} max={today}></input>
+        <button type='submit'>Search</button>
+      </form>
+
       <div className='content'>
-        <img src={data.hdurl} alt='image of the day'></img>
-        <Heading />
+        <img src={data.hdurl} alt='image of the day' onClick={()=> window.open(`${data.hdurl}`, "_blank")}></img>
+        <Heading title={data.title} date={data.date} copyright={data.copyright} explanation={data.explanation} />
       </div>
     </div>
   );
--- src/Heading.js
+++ src/Heading.js
@@ -2,34 +2,15 @@ import React, { useState, useEffect } from "react";
 import "./Heading.css";
 import axios from 'axios';
 
-const url = 'https://api.nasa.gov/planetary/apod';
-const api_key = 'kRfwFSln261qa5kxxPvHAKAHb3azNosA3kLe8dt0';
-
-function Heading(props) {
-  const [data, setData] = useState(null)
-
-  useEffect(() => {
-    axios.get(`${url}?api_key=${api_key}`)
-      .then(res => {
-        console.log(res.data.title);
-        setData(res.data)
-      })
-      .catch(err => {
-        alert('The Heading of the NASA APOD could not be retrieved.')
-      })
-  }, [])
-
-  if(!data) {
-    return null;
-  } 
-  return (
-    <div className="Heading">
-      <h2>{data.title}</h2>
-      <h4>{data.date}</h4>
-      <p className='copyright'>© 2020 <b>{data.copyright}</b></p>
-      <p className='explanation'>{data.explanation}</p>
-    </div>
-  );
+function Heading({ title, date, copyright, explanation }) {
+    return (
+        <div className="Heading">
+            <h2>{title}</h2>
+            <h4>{date}</h4>
+            <p className='explanation'>{explanation}</p>
+            <p className='copyright'>© 2020 <b>{copyright}</b></p>
+        </div>
+    );
 }
 
 export default Heading;
